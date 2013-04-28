cfacbd2f4e100cd791755329d6b41174cf3ddf78
Matt Lea <github@mattleaconsulting.com>
2013-04-28 08:32:17 -0500

    failed merge

--- package.inc.php
+++ package.inc.php
@@ -1,21 +1,27 @@
 <?php
-define('__MLC_HIGHRISE__', dirname(__FILE__));
-define('__MLC_HIGHRISE_CORE__', __MLC_HIGHRISE__ . '/_core');
+/**
+ * Created by JetBrains PhpStorm.
+ * User: user1a
+ * Date: 4/27/13
+ * Time: 7:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+define('__MLC_EVERNOTE__', dirname(__FILE__));
+define('__MLC_EVERNOTE_CORE__', __MLC_EVERNOTE__ . '/_core');
+
+define('__MLC_EVERNOTE_CORE_CTL__', __MLC_EVERNOTE_CORE__ . '/ctl');
+define('__MLC_EVERNOTE_CORE_VIEW__', __MLC_EVERNOTE_CORE__ . '/view');
+define('__MLC_EVERNOTE_CORE_MODEL__', __MLC_EVERNOTE_CORE__ . '/model');
+define('__MLC_EVERNOTE_BATCH__', __MLC_EVERNOTE_CORE__ . '/batch');
+define('__MLC_EVERNOTE_API__', __MLC_EVERNOTE_CORE__ . '/api');
+define('__MLC_EVERNOTE_CG__', __MLC_EVERNOTE__ . '/_codegen');
+require_once(__MLC_EVERNOTE_CORE_MODEL__ . '/_enum.inc.php');
+require_once(__MLC_EVERNOTE_CORE__ . '/_lib/lib/Evernote/Client.php');
 
 
-define('__MLC_HIGHRISE_CORE_CTL__', __MLC_HIGHRISE_CORE__ . '/ctl');
-define('__MLC_HIGHRISE_CORE_MODEL__', __MLC_HIGHRISE_CORE__ . '/model');
-define('__MLC_HIGHRISE_CORE_VIEW__', __MLC_HIGHRISE_CORE__ . '/view');
-define('__MLC_HIGHRISE_CG__', __MLC_HIGHRISE__ . '/_codegen');
-MLCApplicationBase::$arrClassFiles['MLCHRObjectBase'] = __MLC_HIGHRISE_CORE__ . '/MLCHRObjectBase.class.php';
-MLCApplicationBase::$arrClassFiles['MLCHRPeople'] = __MLC_HIGHRISE_CORE__ . '/MLCHRPeople.class.php';
-MLCApplicationBase::$arrClassFiles['MLCHRPeopleData'] = __MLC_HIGHRISE_CORE__ . '/MLCHRPeopleData.class.php';
-MLCApplicationBase::$arrClassFiles['MLCHRTask'] = __MLC_HIGHRISE_CORE__ . '/MLCHRTask.class.php';
-MLCApplicationBase::$arrClassFiles['MLCHREmail'] = __MLC_HIGHRISE_CORE__ . '/MLCHREmail.class.php';
-//OAuth
-MLCApplicationBase::$arrClassFiles['MLCHROAuthDriver'] = __MLC_HIGHRISE_CORE__ . '/MLCHROAuthDriver.class.php';
+MLCApplicationBase::$arrClassFiles['MLCEvernoteDriver'] = __MLC_EVERNOTE_CORE_MODEL__ . '/MLCEvernoteDriver.class.php';
+
+MLCApiEvernote
 
 
-
-require_once(__MLC_HIGHRISE_CORE__ . '/_enum.inc.php');
 
